b81bb0f9684ed6ccea9e42d6d2a4be5f9234998f
galvardi <gal.vardi2@mail.huji.ac.il>
2023-06-20 00:35:20 +0300

    finished yaara tests now on simple test

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <iostream>
 
-struct virtualInfo{
+struct virtualAddress{
     int length;
     uint64_t address;
 };
@@ -16,19 +16,21 @@ struct maxDistPage{
     uint64_t parent_frame;
 };
 
-word_t get_next_frame (virtualInfo &path_info, uint64_t page_swapped_in);
-void get_address (uint64_t &virtualAddress, word_t &dest_frame);
+
 void VMinitialize(){
-  PMwrite (0,0);
-  PMwrite (1,0);
+  for (int i = 0; i < OFFSET_WIDTH; ++i)
+  {
+    PMwrite (i,0);
+  }
 }
 
 unsigned int count_bits(uint64_t number)
 {
-  // log function in base 2
-  // take only integer part
-  // todo check if conversion works well long -> double
-  return (int)log2(number)+1;
+  unsigned int count = 0;
+  for (;number > 0;count++){
+    number = number >> 1;
+  }
+  return count;
 }
 
 int get_root_bits_num(){
@@ -36,26 +38,31 @@ int get_root_bits_num(){
   if (!root_bits_num) root_bits_num = OFFSET_WIDTH;
   return root_bits_num;
 }
-
-
-int split_address(uint64_t& virtualAddress, int shift){
-  int bits_to_shift = count_bits(virtualAddress) - shift;
-  uint64_t prefix = virtualAddress >> bits_to_shift;
-  uint64_t mask = (1LL << (count_bits (virtualAddress) - shift) + 1) - 1;
-  virtualAddress = virtualAddress & mask;
+/*
+ * Returns the prefix of the binary number which is used as the offset
+ * towards the next hirarchy.
+ * Shifts the address number [shift] bits to the right
+ * 001010 ->  01010 returns: 0  (OFFSET = 1)
+ */
+int split_address(virtualAddress& virtual_address, int shift){
+  int bits_to_shift = virtual_address.length - shift;
+  uint64_t prefix = virtual_address.address >> bits_to_shift;
+  uint64_t mask = (1LL << (virtual_address.length - shift)) - 1;
+  virtual_address.length -= shift;
+  virtual_address.address = virtual_address.address & mask;
   return prefix;
 }
 
-bool check_empty_table(uint64_t frame){
-  bool empty = true;
-  word_t val;
-  for (int i = 0; i < OFFSET_WIDTH; ++i)
-  {
-    PMread (frame * PAGE_SIZE + i, &val);
-    empty = empty && val != 0;
-  }
-  return empty;
+uint64_t get_cyclic_dist (virtualAddress &virtual_info, const uint64_t
+&page_swapped_in)
+{
+  uint64_t dist_l = page_swapped_in - virtual_info.address;
+  dist_l = dist_l > 0 ? dist_l : -dist_l;
+  uint64_t dist_r = NUM_PAGES - dist_l;
+  uint64_t cyclic_dist = dist_r < dist_l ? dist_r : dist_l;
+  return cyclic_dist;
 }
+
 
 /**
  * returns the empty table child if exists else 0
@@ -67,39 +74,40 @@ bool check_empty_table(uint64_t frame){
  * @return
  */
 uint64_t traverse(uint64_t frame_pointer,
-                  virtualInfo virtual_info,
+                  virtualAddress virtual_info,
                   uint64_t parent_frame,
                   uint64_t& max_frame_index,
                   uint64_t& page_swapped_in,
-                  maxDistPage& max_page){
-    if (virtual_info.length == VIRTUAL_ADDRESS_WIDTH)
+                  maxDistPage& max_page,
+                  uint64_t last_frame){
+    if (virtual_info.length == VIRTUAL_ADDRESS_WIDTH - OFFSET_WIDTH)
     {
-      // check max_dist and return;
-      uint64_t dist_l = page_swapped_in - virtual_info.address;
-      dist_l = dist_l > 0 ? dist_l : -dist_l;
-      uint64_t dist_r = NUM_PAGES - dist_l;
-      uint64_t cyclic_dist = dist_r < dist_l ? dist_r : dist_l;
+      // check max_dist;
+      uint64_t cyclic_dist = get_cyclic_dist (virtual_info, page_swapped_in);
       if (cyclic_dist > max_page.distance){
         max_page.distance = cyclic_dist;
         max_page.frame_pointer = frame_pointer;
         max_page.v_address = virtual_info.address;
         max_page.parent_frame = parent_frame;
       }
+      return 0;
     }
     bool empty_table = true;
     word_t offset = OFFSET_WIDTH;
     if (virtual_info.length == 0){
       offset = get_root_bits_num();
     }
-    for (int i = 0; i < offset; ++i){
+    uint64_t loop_offset = 1LL << offset;
+    for (int i = 0; i < loop_offset; ++i){
       word_t child_p_frame;
       PMread(frame_pointer * PAGE_SIZE + i, &child_p_frame);
       max_frame_index = child_p_frame > max_frame_index ? child_p_frame :
                         max_frame_index; //todo check
       if (child_p_frame != 0){
         empty_table = false;
-        uint64_t child_virtual_address = (virtual_info.length << offset) + i;
-        virtualInfo child_virtual_info = {
+        if (child_p_frame == last_frame) continue;
+        uint64_t child_virtual_address = (virtual_info.address << offset) + i;
+        virtualAddress child_virtual_info = {
             virtual_info.length + offset,
             child_virtual_address
         };
@@ -108,79 +116,92 @@ uint64_t traverse(uint64_t frame_pointer,
                                                frame_pointer,
                                                max_frame_index,
                                                page_swapped_in,
-                                               max_page);
+                                               max_page,
+                                               last_frame);
         // if empty table found during traverse
         if (empty_table_index) {
           // unlink empty child if it's the father
           if (empty_table_index == child_p_frame){
-            PMwrite (frame_pointer + i, 0);
+            PMwrite (frame_pointer * PAGE_SIZE + i, 0);
           }
           return empty_table_index;
         }
       }
-      if (empty_table){
-        return frame_pointer;
-      }
+    }
+    if (empty_table){
+      return frame_pointer;
     }
     return 0;
   }
 
-uint64_t get_address (uint64_t &virtualAddress, uint64_t& dest_frame)
-{
-  int root_bits_num = get_root_bits_num();
-  int shift = root_bits_num;
-  uint64_t offset_size = 1LL << OFFSET_WIDTH;
-  word_t next_frame;
-  virtualInfo path_info = {0, 0};
-  uint64_t page_swapped_in = virtualAddress >> OFFSET_WIDTH;
-  while (virtualAddress > (offset_size)){
-    uint64_t offset = split_address (virtualAddress, shift);
-    PMread(dest_frame * PAGE_SIZE + offset, &next_frame);
-    // Page Fault
-    if (next_frame == 0){
-      next_frame = get_next_frame (path_info, page_swapped_in);
-      // write the next_frame
-      PMwrite (dest_frame + offset, next_frame);
-      // init the next_frame table
-      if (count_bits (virtualAddress) > OFFSET_WIDTH*2){
-        for (int i = 0; i < PAGE_SIZE; i++){
-          PMwrite (next_frame + i, 0);
-        }
-      }
-    }
-    dest_frame = next_frame;
-  }
-  return dest_frame * PAGE_SIZE + virtualAddress;
-}
-word_t get_next_frame (virtualInfo &path_info, uint64_t page_swapped_in)
+
+
+word_t get_next_frame (virtualAddress &path_info,
+                       uint64_t page_swapped_in,
+                       uint64_t last_frame)
 {
   word_t next_frame;
   uint64_t max_frame_index = 0;
   maxDistPage max_page = {0,0,0};
   uint64_t empty_table_index = traverse (0, path_info, 0,
-                                        max_frame_index,
-                                        page_swapped_in,
-                                        max_page);
+                                         max_frame_index,
+                                         page_swapped_in,
+                                         max_page,
+                                         last_frame);
   // 1. empty table
   if (empty_table_index){
     next_frame = empty_table_index;
   }
-  // 2. unused frame
+    // 2. unused frame
   else if (max_frame_index + 1 < NUM_FRAMES){
     next_frame = max_frame_index + 1;
   }
-  // 3. swap out a page
+    // 3. swap out a page
   else{
     // evict
-    PMevict(max_page.v_address, max_page.frame_pointer * PAGE_SIZE);
+    PMevict(max_page.frame_pointer, max_page.v_address);
     // todo: unlink father
     uint64_t page_link_address = max_page.parent_frame * PAGE_SIZE + (max_page
-        .v_address % PAGE_SIZE);
+                                                                          .v_address % PAGE_SIZE);
     PMwrite(page_link_address, 0);
     next_frame = max_page.frame_pointer;
   }
   return next_frame;
 }
+
+
+uint64_t get_address (uint64_t &_virtual_address, uint64_t& dest_address)
+{
+  int root_bits_num = get_root_bits_num();
+  int shift = root_bits_num;
+  uint64_t cur_frame = 0;
+  word_t next_frame;
+  virtualAddress path_info = {0, 0};
+  virtualAddress virtual_address = {VIRTUAL_ADDRESS_WIDTH, _virtual_address};
+  uint64_t page_swapped_in = _virtual_address >> OFFSET_WIDTH;
+  uint64_t op_frame = 0;
+  while (virtual_address.length > OFFSET_WIDTH){
+    uint64_t offset = split_address (virtual_address, shift);
+    PMread(cur_frame * PAGE_SIZE + offset, &next_frame);
+    // Page Fault
+    if (next_frame == 0){
+      next_frame = get_next_frame (path_info, page_swapped_in, op_frame);
+      op_frame = next_frame;
+      // write the next_frame
+      PMwrite (cur_frame * PAGE_SIZE + offset, next_frame);
+      // init the next_frame table
+      if (virtual_address.length >= OFFSET_WIDTH * 2){
+        for (int i = 0; i < PAGE_SIZE; i++){
+          PMwrite (next_frame * PAGE_SIZE + i, 0);
+        }
+      }
+    }
+    else op_frame = 0;
+    cur_frame = next_frame;
+  }
+  dest_address = cur_frame * PAGE_SIZE + virtual_address.address;
+}
+
 
 
 int VMread(uint64_t virtualAddress, word_t* value){ //todo return success/ fail
@@ -191,7 +212,7 @@ int VMread(uint64_t virtualAddress, word_t* value){ //todo return success/ fail
   // read the page
   PMread(dest_address, value);
   // todo: return success / failure
-  std::cout << "read " << value << std::endl;
+//  std::cout << "read " << value << std::endl;
   return 0;
 }
 
@@ -200,7 +221,7 @@ int VMwrite(uint64_t virtualAddress, word_t value){ //todo return success/ fail
   get_address (virtualAddress, dest_address);
   PMwrite (dest_address, value);
   // todo: return success / failure
-  std::cout << "write to " << value << std::endl;
+//  std::cout << "write " << "value" << " to " << value << std::endl;
   return 0;
 }
 
